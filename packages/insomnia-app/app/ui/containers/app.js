f952b7b1b88a9299fd3d8c89e7be2cd0d89190ff
Opender Singh <opender.singh@konghq.com>
2021-01-28 16:41:36 +1300

    chore: remove APP_ID_INSOMNIA where unnecessary

--- packages/insomnia-app/app/ui/containers/app.js
+++ packages/insomnia-app/app/ui/containers/app.js
@@ -21,6 +21,7 @@ import {
   DEFAULT_PANE_HEIGHT,
   DEFAULT_PANE_WIDTH,
   DEFAULT_SIDEBAR_WIDTH,
+  getAppName,
   MAX_PANE_HEIGHT,
   MAX_PANE_WIDTH,
   MAX_SIDEBAR_REMS,
@@ -28,8 +29,6 @@ import {
   MIN_PANE_WIDTH,
   MIN_SIDEBAR_REMS,
   PREVIEW_MODE_SOURCE,
-  getAppId,
-  getAppName,
 } from '../../common/constants';
 import * as globalActions from '../redux/modules/global';
 import * as entitiesActions from '../redux/modules/entities';
@@ -64,9 +63,9 @@ import RequestRenderErrorModal from '../components/modals/request-render-error-m
 import * as network from '../../network/network';
 import {
   debounce,
+  generateId,
   getContentDispositionHeader,
   getDataDirectory,
-  generateId,
 } from '../../common/misc';
 import * as mime from 'mime-types';
 import * as path from 'path';
@@ -80,6 +79,7 @@ import KeydownBinder from '../components/keydown-binder';
 import ErrorBoundary from '../components/error-boundary';
 import * as plugins from '../../plugins';
 import * as templating from '../../templating/index';
+import { NUNJUCKS_TEMPLATE_GLOBAL_PROPERTY_NAME } from '../../templating/index';
 import AskModal from '../components/modals/ask-modal';
 import { updateMimeType } from '../../models/request';
 import MoveRequestGroupModal from '../components/modals/move-request-group-modal';
@@ -93,8 +93,6 @@ import NeDBPlugin from '../../sync/git/ne-db-plugin';
 import FSPlugin from '../../sync/git/fs-plugin';
 import { routableFSPlugin } from '../../sync/git/routable-fs-plugin';
 import AppContext from '../../common/strings';
-import { APP_ID_INSOMNIA } from '../../../config';
-import { NUNJUCKS_TEMPLATE_GLOBAL_PROPERTY_NAME } from '../../templating/index';
 import { isGrpcRequest, isGrpcRequestId, isRequestGroup } from '../../models/helpers/is-model';
 import * as requestOperations from '../../models/helpers/request-operations';
 import { GrpcProvider } from '../context/grpc';
@@ -1003,7 +1001,8 @@ class App extends PureComponent {
     if (activity === ACTIVITY_HOME) {
       title = getAppName();
     } else {
-      title = getAppId() === APP_ID_INSOMNIA ? activeWorkspace.name : activeApiSpec.fileName;
+      title =
+        activeWorkspace.scope === 'collection' ? activeWorkspace.name : activeApiSpec.fileName;
       if (activeEnvironment) {
         title += ` (${activeEnvironment.name})`;
       }
